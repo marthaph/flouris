2671f0f3392dae919112c1b325739eb04194442f
marthaph <ravskiy32@gmail.com>
2016-02-06 19:19:18 +0200

    add main files

---
+++ js/function.js
@@ -0,0 +1,8 @@
+$(document).ready(function() { 
+	$('#our').fadeIn(1500).delay(7500).fadeOut(1000);
+    $('#one').fadeIn(1500).delay(500).fadeOut(1000);
+    $('#two').delay(3000).fadeIn(1500).delay(500).fadeOut(1000);
+    $('#thre').delay(6000).fadeIn(1500).delay(500).fadeOut(1000);
+    $('#logo').delay(8500).fadeIn(1500);
+    $('#text_logo').delay(10000).fadeIn(1500);
+});
